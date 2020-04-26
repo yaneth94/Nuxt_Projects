3d51b029f3936593b39f0802adcccdc216181e3c
Zoila <zoilavillatoro6694@outlook.com>
2020-04-26 13:18:53 -0600

    started project nuxt

---
+++ nuxt-workshop/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+         <h1>Albums de la página</h1>
+        <div v-for="album in albums" :key="album.id">
+            <h3>{{album.title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import env from '../config/env'
+
+export default {
+    name: 'IndexPage',
+    data(){
+        return{
+            albums: []
+        }
+    },
+    created(){
+        axios.get(`${env.endpoint}/albums`).then(response => {
+            console.log(response.data)
+            this.albums = response.data
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
